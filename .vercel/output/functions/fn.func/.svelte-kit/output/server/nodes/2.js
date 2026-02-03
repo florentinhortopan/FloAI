5dc6b387cbad9fd753c5566ae88c576a30ff035a
Florentinhortopan <flo@puxa.ai>
2026-02-02 16:53:44 -0800

    Add .vercel to gitignore

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.C2SLujzj.js","_app/immutable/chunks/CgT0rgf2.js","_app/immutable/chunks/C6B6ZOh-.js","_app/immutable/chunks/CXOmKNHw.js"];
-export const stylesheets = ["_app/immutable/assets/2.KDtfBf9E.css"];
-export const fonts = [];
